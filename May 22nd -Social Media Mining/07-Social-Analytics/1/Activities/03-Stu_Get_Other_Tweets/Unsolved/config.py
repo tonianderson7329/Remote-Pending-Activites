cf62296e0db07d194f132331ff815c2356a4b457
tonianderson7329 <33438190+tonianderson7329@users.noreply.github.com>
2018-09-25 12:27:52 -0500

    Add files via upload

---
+++ May 22nd -Social Media Mining/07-Social-Analytics/1/Activities/03-Stu_Get_Other_Tweets/Unsolved/config.py
@@ -0,0 +1,6 @@
+#  Add your API key
+weather_key = "a454a2728155fbb6430895d8d4f88fc7"
+consumer_key = "MrupRy3geGb0jTNjjwuwFhBrg"
+consumer_secret = "IKDrnkPSlHTBF8ZjNY2FsQ6H5sMt0ix9WTP2C0k9eziYvPCaRA"
+access_token = "796235210-InuqgfdlvYZS07bNkoI3vanGq4cFDPDalR82ZEjY"
+access_secret = "8Yk31Q7wo6tecfnNwanTpO1sRObVdo73Q4X2YPX3x4eJ9"
