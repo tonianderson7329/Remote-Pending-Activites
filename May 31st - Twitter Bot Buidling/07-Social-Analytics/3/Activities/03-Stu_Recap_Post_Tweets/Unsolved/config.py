129c0ba28cc5c689fdd07e993fbcf8e00eb05413
tonianderson7329 <33438190+tonianderson7329@users.noreply.github.com>
2018-09-25 12:29:04 -0500

    Add files via upload

---
+++ May 31st - Twitter Bot Buidling/07-Social-Analytics/3/Activities/03-Stu_Recap_Post_Tweets/Unsolved/config.py
@@ -0,0 +1,5 @@
+#  Add your API key
+consumer_key = "MrupRy3geGb0jTNjjwuwFhBrg"
+consumer_secret = "IKDrnkPSlHTBF8ZjNY2FsQ6H5sMt0ix9WTP2C0k9eziYvPCaRA"
+access_token = "796235210-InuqgfdlvYZS07bNkoI3vanGq4cFDPDalR82ZEjY"
+access_secret = "8Yk31Q7wo6tecfnNwanTpO1sRObVdo73Q4X2YPX3x4eJ9"
